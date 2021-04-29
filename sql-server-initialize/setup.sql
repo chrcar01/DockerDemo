ac71195cfe633232384ae305ce061c55ba7abf70
chrcar01 <chrisjcarter@gmail.com>
2021-04-28 19:07:05 -0700

    added app login,user

--- sql-server-initialize/setup.sql
+++ sql-server-initialize/setup.sql
@@ -1,7 +1,15 @@
-CREATE DATABASE [OrdersDemo]
+USE [master]
+GO
+
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE [name]='OrdersDemo')
+	CREATE DATABASE [OrdersDemo]
 GO
 
 USE [OrdersDemo]
+GO
+
+IF OBJECT_ID('Customer') IS NOT NULL
+	DROP TABLE [Customer]
 GO
 
 CREATE TABLE [Customer](
@@ -9,4 +17,26 @@ CREATE TABLE [Customer](
   [FirstName] VARCHAR(50) NOT NULL,
   [LastName] VARCHAR(50) NOT NULL
 )
+GO
+
+IF EXISTS (SELECT * from [master]..syslogins WHERE [name] = 'OrdersApp')
+	DROP LOGIN [OrdersApp]
+GO
+
+CREATE LOGIN [OrdersApp] WITH PASSWORD='Password@12345'
+GO
+
+IF EXISTS (SELECT * FROM sysusers WHERE [name] = 'OrdersApp')
+	DROP USER [OrdersApp]
+GO
+
+CREATE USER [OrdersApp] FOR LOGIN [OrdersApp]
+GO
+
+ALTER ROLE db_datareader
+  ADD MEMBER OrdersApp
+GO
+
+ALTER ROLE db_datawriter
+  ADD MEMBER OrdersApp
 GO
